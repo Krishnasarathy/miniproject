bb5ba33ad79bdcfae7d778dd48c0d0e1d0155cd9
Krishnasarathy <109368726+Krishnasarathy@users.noreply.github.com>
2023-07-10 20:06:13 +0530

    Add files via upload

---
+++ Day_02/Login_page/src/Components/Nav.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+export default function Nav() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label=''
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+            </MDBNavbarItem>
+
+            <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                  Dropdown
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem link>Action</MDBDropdownItem>
+                  <MDBDropdownItem link>Another action</MDBDropdownItem>
+                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavbarItem>
+          </MDBNavbarNav>   
+          <form className='d-flex input-group w-auto'>
+            <input type='search' className='form-control px-5' placeholder='Type query' aria-label='Search' />
+            <button className="btn btn-primary  px-2" type="button">Search</button>
+          </form>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
---
+++ Day_02/Login_page/src/Pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Home.css';
+import Nav from '../Components/Nav';
+import Card from '../Components/Card';
+
+const Home = () => {
+  return (
+    <><Nav /><div>
+
+
+      </div></>
+
+
+
+  )
+}
+
+export default Home
